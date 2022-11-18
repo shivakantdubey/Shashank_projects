e14974e0217220d10b7059ff684ba2df242d72f1
Shivakant Dubey <shiva11496@gmail.com>
2022-11-18 17:00:11 +0530

    S

--- app/src/main/java/in/booksgo/shashank_project/Login.java
+++ app/src/main/java/in/booksgo/shashank_project/Login.java
@@ -22,11 +22,7 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView username =(TextView) findViewById(R.id.username);
         TextView password =(TextView) findViewById(R.id.password);
-
-
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        //admin and admin
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
