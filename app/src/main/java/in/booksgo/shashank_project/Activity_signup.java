4e5e025da647d07c4bd108ae57662ff559ea634e
Shivakant Dubey <shiva11496@gmail.com>
2022-11-22 12:43:25 +0530

    S

---
+++ app/src/main/java/in/booksgo/shashank_project/Activity_signup.java
@@ -0,0 +1,14 @@
+package in.booksgo.shashank_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
